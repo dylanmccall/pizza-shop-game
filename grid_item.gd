b567a7d83f1e221fd8c5569f99ba976f3d74384a
Dylan McCall <dylan@endlessos.org>
2024-05-08 13:51:22 -0700

    grid_item: Emit a "touched" signal on press

--- grid_item.gd
+++ grid_item.gd
@@ -1,3 +1,5 @@
+class_name GridItem
+
 extends Node2D
 
 @export var is_selected:bool = false:
@@ -9,3 +11,13 @@ func _set_is_selected(value:bool):
 	is_selected = value
 	$SelectionSprite.visible = value
 	queue_redraw()
+
+func _on_input_event(viewport, event, shape_idx):
+	if not event.is_action("touch"):
+		return
+
+	if not event.is_pressed():
+		return
+
+	grid_item_touched.emit(self)
+	get_viewport().set_input_as_handled()
