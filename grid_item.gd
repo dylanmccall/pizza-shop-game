d18b532b6df0c3b2ffb6fecb403158986d664ead
Dylan McCall <dylan@endlessos.org>
2024-05-08 13:49:32 -0700

    grid_item: Add is_selected property

--- grid_item.gd
+++ grid_item.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+@export var is_selected:bool = false:
+		set = _set_is_selected
+
+signal grid_item_touched(source)
+
+func _set_is_selected(value:bool):
+	is_selected = value
+	$SelectionSprite.visible = value
+	queue_redraw()
