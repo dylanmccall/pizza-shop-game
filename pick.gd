b3fc62d4885224bd1b6b6189e5186ac98fb33176
Dan Nicholson <dbn@endlessos.org>
2024-05-10 11:11:07 -0600

    Add order and status labels

--- pick.gd
+++ pick.gd
@@ -1,21 +1,33 @@
 extends Node2D
 
 @onready var pizza = $Pizza2
+@onready var order_label = $Order
+@onready var status_label = $Status
 var order: int
 
 
-func reset():
+func reset_pizza():
+	status_label.text = ""
 	pizza.reset()
+
+
+func reset_order():
 	order = Toppings.get_random_order()
-	print_order()
+	set_order_label()
 
 
-func print_order():
+func reset():
+	reset_pizza()
+	reset_order()
+
+
+func set_order_label():
 	var names = []
 	for type in Toppings.Type.values():
 		if order & (1 << type):
 			names.append(Toppings.get_type_name(type))
 	print("Order: %s" % ", ".join(names))
+	order_label.text = "Order:\n%s" % "\n".join(names)
 
 
 func _ready():
@@ -24,11 +36,15 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("new"):
-		pizza.reset()
+		reset_pizza()
 
 
 func _on_topping_picked(topping):
 	pizza.add_topping(topping)
 	if pizza.toppings == order:
+		status_label.text = "READY!"
 		print("You win!")
+		await get_tree().create_timer(1).timeout
 		reset()
+	elif pizza.toppings & ~order:
+		status_label.text = "WRONG!"
