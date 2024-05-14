48453dce0cc08b56193323cf9d818e1538af245d
Dylan McCall <dylan@endlessos.org>
2024-05-14 10:59:37 -0700

    grid: Add editor support
    The grid cell nodes will now appear in the editor. To improve this
    experience, arrange them around a center point.

--- grid.gd
+++ grid.gd
@@ -1,9 +1,11 @@
+@tool
 class_name Grid
-extends Area2D
+extends Node2D
 
-@export var TILE_SIZE:Vector2 = Vector2(64, 64)
-@export var WIDTH:int = 8
-@export var HEIGHT:int = 8
+const TILE_SIZE:Vector2 = Vector2(64, 64)
+
+@export var GRID_SIZE:Vector2 = Vector2(9, 9):
+	set = _set_grid_size
 
 var grid_cell_scene:Resource = load("res://grid_cell.tscn")
 
@@ -13,19 +15,11 @@ var tomato_scene:Resource = load("res://grid_items/tomato_grid_item.tscn")
 var selected_grid_cell:GridCell = null
 
 func _ready():
-	for index in range(0, WIDTH * HEIGHT):
-		var grid_cell = grid_cell_scene.instantiate()
-		grid_cell.position = get_coordinate_from_index(index) * TILE_SIZE
-		grid_cell.connect("grid_cell_pressed", self._on_grid_cell_pressed)
-		grid_cell.connect("grid_cell_unpressed", self._on_grid_cell_unpressed)
-		$GridCells.add_child(grid_cell)
+	update_grid_cells()
 
-	get_grid_cell_from_index(1).put_grid_item(cheese_scene.instantiate())
-	get_grid_cell_from_index(2).put_grid_item(cheese_scene.instantiate())
-	get_grid_cell_from_index(3).put_grid_item(tomato_scene.instantiate())
-	get_grid_cell_from_index(5).put_grid_item(tomato_scene.instantiate())
-	get_grid_cell_from_index(6).put_grid_item(tomato_scene.instantiate())
-	get_grid_cell_from_index(9).put_grid_item(cheese_scene.instantiate())
+func _set_grid_size(value:Vector2):
+	GRID_SIZE = value
+	update_grid_cells()
 
 func _on_grid_cell_pressed(grid_cell:GridCell):
 	if grid_cell == selected_grid_cell:
@@ -56,9 +50,35 @@ func _on_grid_cell_unpressed(grid_cell:GridCell):
 
 	deselect_grid_cell()
 
+func update_grid_cells():
+	for node in $GridCells.get_children():
+		$GridCells.remove_child(node)
+		node.queue_free()
+
+	# This looks better in the editor if we arrange cells around a center point.
+	var center = (GRID_SIZE - Vector2.ONE) / 2
+
+	for index in range(0, GRID_SIZE.x * GRID_SIZE.y):
+		var grid_coordinate = get_coordinate_from_index(index)
+		var grid_cell = grid_cell_scene.instantiate()
+		grid_cell.position = (grid_coordinate - center) * TILE_SIZE
+		if not Engine.is_editor_hint():
+			grid_cell.connect("grid_cell_pressed", self._on_grid_cell_pressed)
+			grid_cell.connect("grid_cell_unpressed", self._on_grid_cell_unpressed)
+		$GridCells.add_child(grid_cell)
+
+	if not Engine.is_editor_hint():
+		get_grid_cell_from_index(1).put_grid_item(cheese_scene.instantiate())
+		get_grid_cell_from_index(2).put_grid_item(cheese_scene.instantiate())
+		get_grid_cell_from_index(3).put_grid_item(tomato_scene.instantiate())
+		get_grid_cell_from_index(5).put_grid_item(tomato_scene.instantiate())
+		get_grid_cell_from_index(6).put_grid_item(tomato_scene.instantiate())
+		get_grid_cell_from_index(9).put_grid_item(cheese_scene.instantiate())
+
 func get_coordinate_from_index(index:int) -> Vector2:
-	var index_x = index / WIDTH
-	var index_y = index % HEIGHT
+	@warning_ignore("integer_division")
+	var index_x = index % int(GRID_SIZE.x)
+	var index_y = index / int(GRID_SIZE.x)
 	return Vector2(index_x, index_y)
 
 func get_coordinate_from_grid_cell(grid_cell:GridCell) -> Vector2:
